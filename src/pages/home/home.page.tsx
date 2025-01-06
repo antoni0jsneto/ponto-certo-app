de5bd136f0391ca6f9d93be85a363189ebae5010
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-06 18:30:42 -0300

    feat: added Container and Card Component for Home Page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,10 +1,18 @@
 // Components
+import Card from '../../components/card/card.component';
+import Container from '../../components/container/container.component';
 import Header from '../../components/header/header.component';
+import QuickAccess from '../../components/home/quick-access.component';
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+      <Container>
+        <Card width="100%">
+          <QuickAccess />
+        </Card>
+      </Container>
     </div>
   );
 };
