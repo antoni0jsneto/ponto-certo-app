8dc4f27575d1b9db4d3d9c5b26ff53936a4a7faa
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-06 14:19:33 -0300

    feat: add Financial Overview Page

---
+++ src/pages/home/financial-overview.page.tsx
@@ -0,0 +1,12 @@
+// Components
+import Header from '../../components/header/header.component';
+
+const FinancialOverviewPage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default FinancialOverviewPage;
