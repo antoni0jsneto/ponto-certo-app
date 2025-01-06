ed6368fbdd785e9b022380dfd0a25acd4b26e54f
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-06 18:27:14 -0300

    style: added new style setting to main div

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -5,28 +5,37 @@ import { CgSpinnerTwo } from 'react-icons/cg';
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <h2 className="header-title">
-        <CgSpinnerTwo size={30} />
-        PONTO CERTO APP
-      </h2>
-      <div className="header-items-container">
-        <div className="header-items">
-          <div className="header-item">visão geral</div>
-          <div className="header-item">lançamentos</div>
-          <div className="header-item">relatórios</div>
-          <div className="header-item">metas</div>
-        </div>
-        <div className="header-items">
-          <div className="header-item">
-            <FaGear size={20} />
+    <div
+      style={{
+        backgroundColor: 'var(--header-background-color)',
+        display: 'flex',
+        justifyContent: 'center',
+        marginBottom: '2rem',
+      }}
+    >
+      <div className="header-container">
+        <h2 className="header-title">
+          <CgSpinnerTwo size={30} />
+          PONTO CERTO APP
+        </h2>
+        <div className="header-items-container">
+          <div className="header-items">
+            <div className="header-item">visão geral</div>
+            <div className="header-item">lançamentos</div>
+            <div className="header-item">relatórios</div>
+            <div className="header-item">metas</div>
           </div>
-          <div className="header-item">
-            <IoMdNotifications size={22} />
-            <p className="notification-icon">5</p>
-          </div>
+          <div className="header-items">
+            <div className="header-item">
+              <FaGear size={20} />
+            </div>
+            <div className="header-item">
+              <IoMdNotifications size={22} />
+              <p className="notification-icon">5</p>
+            </div>
 
-          {/* Perfil aqui */}
+            {/* Perfil aqui */}
+          </div>
         </div>
       </div>
     </div>
