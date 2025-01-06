45be02c7be14c18ec0ca94977246b3429d095834
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-06 14:20:05 -0300

    feat: add Goals Page

---
+++ src/pages/home/goals.page.tsx
@@ -0,0 +1,12 @@
+// Components
+import Header from '../../components/header/header.component';
+
+const GoalsPage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default GoalsPage;
