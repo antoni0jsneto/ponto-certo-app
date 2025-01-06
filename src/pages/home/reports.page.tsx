6261bb9d2d3ac05de2badfb51b8457827cefb11b
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-06 14:20:21 -0300

    feat: add Reports Page

---
+++ src/pages/home/reports.page.tsx
@@ -0,0 +1,12 @@
+// Components
+import Header from '../../components/header/header.component';
+
+const ReportsPage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default ReportsPage;
