a5fb02a8814d269ff27e0970cdd817b5fc3ef6a2
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-07 18:18:46 -0300

    feat: add Quick Access Component

--- src/components/home/quick-access.component.tsx
+++ src/components/home/quick-access.component.tsx
@@ -1,21 +1,58 @@
-import Card from '../card/card.component';
-import Container from '../container/container.component';
+// Styles
 import './quick-access.styles.css';
+
+// Utilities
+import { FiMinusCircle } from 'react-icons/fi';
+import { LuCirclePlus } from 'react-icons/lu';
+import { BiTransfer } from 'react-icons/bi';
+import { TfiBarChart } from 'react-icons/tfi';
+
+// Component
+import QuickCustomButton from '../quick-custom-button/quick-custom-button.component';
 
 const QuickAccess = () => {
   return (
-    <div>
-      <p>Boa tarde,</p>
-      <p>Neto!</p>
+    <div className="quick-access-container">
+      <div className="quick-access-content">
+        <p className="quick-access-title">Boa tarde,</p>
+        <p className="quick-access-subtitle">Neto!</p>
 
-      <div>
-        <div>
-          <p>receita mensal</p>
-          <p>R$ 0,00</p>
+        <div className="blocks-of-info-container">
+          <div className="block-of-info-content">
+            <p>receita mensal</p>
+            <p className="block-of-info-positive-value">R$ 0,00</p>
+          </div>
+          <div className="block-of-info-content">
+            <p>despesa mensal</p>
+            <p className="block-of-info-negative-value">R$ 0,00</p>
+          </div>
         </div>
-        <div>
-          <p>despesa mensal</p>
-          <p>R$ 0,00</p>
+      </div>
+
+      <div className="quick-access-content">
+        <p>Acesso rápido</p>
+        <div className="block-of-buttons-content">
+          <QuickCustomButton
+            startIcon={<FiMinusCircle />}
+            iconColor="var(--red-color)"
+          >
+            Despesa
+          </QuickCustomButton>
+          <QuickCustomButton
+            startIcon={<LuCirclePlus />}
+            iconColor="var(--green-color)"
+          >
+            Receita
+          </QuickCustomButton>
+          <QuickCustomButton startIcon={<BiTransfer />}>
+            Transf.
+          </QuickCustomButton>
+          <QuickCustomButton
+            startIcon={<TfiBarChart />}
+            iconColor="var(--blue-color)"
+          >
+            Relat.
+          </QuickCustomButton>
         </div>
       </div>
     </div>
