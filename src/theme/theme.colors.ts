8a9d17bffdd041d3f97cce345217b62cda075e6d
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-07 18:17:35 -0300

    feat: add colors theme

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,19 @@
+const Colors = {
+  primary: '#212529',
+  text: {
+    white: '#f8f9fa',
+    dark: '#343a40',
+  },
+  background: {
+    dark: '#212529',
+    white: '#f8f9fa',
+  },
+  input: {
+    background: '#E9ECEF',
+    placeholder: '#6C757D',
+  },
+  error: '#FF6A6A',
+  success: '#198754',
+};
+
+export default Colors;
