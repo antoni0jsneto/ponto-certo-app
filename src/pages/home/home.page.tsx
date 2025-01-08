2e7ee79c57d78ce305c5d7683f9f2e23521f54cf
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-08 14:19:26 -0300

    feat: added new components to the home page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -3,6 +3,8 @@ import Card from '../../components/card/card.component';
 import Container from '../../components/container/container.component';
 import Header from '../../components/header/header.component';
 import QuickAccess from '../../components/home/quick-access.component';
+import MyAccounts from '../../components/my-accounts/my-accounts.component';
+import MyCreditCards from '../../components/my-credit-cards/my-credit-cards.component';
 
 const HomePage = () => {
   return (
@@ -11,6 +13,12 @@ const HomePage = () => {
       <Container>
         <Card width="100%">
           <QuickAccess />
+        </Card>
+        <Card width="50%">
+          <MyAccounts />
+        </Card>
+        <Card width="50%">
+          <MyCreditCards />
         </Card>
       </Container>
     </div>
