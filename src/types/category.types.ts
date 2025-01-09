a396594851a0477e71c1feccc46a3b8d52004e8b
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-09 15:09:12 -0300

    feat: add Category types

---
+++ src/types/category.types.ts
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+
+interface Category {
+  color: string;
+  background: string;
+  icon: ReactNode;
+  title: string;
+  type: string;
+}
+
+export default Category;
