5fddc2c1ef04f5a60b49681a3bae4b0b980e5f3f
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-09 16:39:39 -0300

    feat: add Home Page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -5,8 +5,123 @@ import Header from '../../components/header/header.component';
 import QuickAccess from '../../components/home/quick-access.component';
 import MyAccounts from '../../components/my-accounts/my-accounts.component';
 import MyCreditCards from '../../components/my-credit-cards/my-credit-cards.component';
+import NextAccounts from '../../components/next-accounts/next-accounts.component';
+
+// Utilities
+import NextAccount from '../../types/next-account.types';
+import Category from '../../types/category.types';
+
+import { PiForkKnifeFill } from 'react-icons/pi';
+import { FaLaptopCode } from 'react-icons/fa6';
+import { BiSolidDrink } from 'react-icons/bi';
+import { GoStarFill } from 'react-icons/go';
+import { AiFillHome } from 'react-icons/ai';
 
 const HomePage = () => {
+  const categories: Category[] = [
+    {
+      color: '#ffffff',
+      background: '#e274a8',
+      icon: <PiForkKnifeFill />,
+      title: 'Alimentação',
+      type: 'expense',
+    },
+    {
+      color: '#ffffff',
+      background: '#c671e4',
+      icon: <FaLaptopCode />,
+      title: 'Assinaturas e serviços',
+      type: 'expense',
+    },
+    {
+      color: '#ffffff',
+      background: '#626491',
+      icon: <BiSolidDrink />,
+      title: 'Bares e restaurantes',
+      type: 'expense',
+    },
+    {
+      color: '#ffffff',
+      background: '#8193ff',
+      icon: <AiFillHome />,
+      title: 'Casa',
+      type: 'expense',
+    },
+    {
+      color: '#ffffff',
+      background: '#2EE8B3',
+      icon: <GoStarFill />,
+      title: 'Salário',
+      type: 'income',
+    },
+  ];
+
+  const nextAccounts: NextAccount[] = [
+    {
+      title: 'salario',
+      date: '05/02/2025',
+      value: 30000,
+      category: {
+        color: '#ffffff',
+        background: '#2EE8B3',
+        icon: <GoStarFill />,
+        title: 'Salário',
+        type: 'income',
+      },
+    },
+    {
+      title: 'salario',
+      date: '05/03/2025',
+      value: 30000,
+      category: {
+        color: '#ffffff',
+        background: '#2EE8B3',
+        icon: <GoStarFill />,
+        title: 'Salário',
+        type: 'income',
+      },
+    },
+    {
+      title: 'salario',
+      date: '05/04/2025',
+      value: 30000,
+      category: {
+        color: '#ffffff',
+        background: '#2EE8B3',
+        icon: <GoStarFill />,
+        title: 'Salário',
+        type: 'income',
+      },
+    },
+  ];
+
+  const expenses: NextAccount[] = [
+    {
+      title: 'condomínio',
+      date: '10/01/2025',
+      value: 500,
+      category: {
+        color: '#ffffff',
+        background: '#8193ff',
+        icon: <AiFillHome />,
+        title: 'Casa',
+        type: 'expense',
+      },
+    },
+    {
+      title: 'luz',
+      date: '10/01/2025',
+      value: 250,
+      category: {
+        color: '#ffffff',
+        background: '#8193ff',
+        icon: <AiFillHome />,
+        title: 'Casa',
+        type: 'expense',
+      },
+    },
+  ];
+
   return (
     <div>
       <Header />
@@ -16,6 +131,7 @@ const HomePage = () => {
         </Card>
       </Container>
       <Container>
+        {/* left div */}
         <div
           style={{
             width: 'calc(50% - 1rem)',
@@ -24,7 +140,12 @@ const HomePage = () => {
           <Card width="100%">
             <MyAccounts />
           </Card>
+          <Card width="100%">
+            <NextAccounts title="Contas a receber" itens={nextAccounts} />
+          </Card>
         </div>
+
+        {/* right div */}
         <div
           style={{
             width: '50%',
@@ -32,6 +153,9 @@ const HomePage = () => {
         >
           <Card width="100%">
             <MyCreditCards />
+          </Card>
+          <Card width="100%">
+            <NextAccounts title="Contas a pagar" itens={expenses} />
           </Card>
         </div>
       </Container>
