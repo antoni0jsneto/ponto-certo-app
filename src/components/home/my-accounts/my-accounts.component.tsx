73b115cf0010a33505a1d96b8613c24e712cc8d5
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-14 12:12:30 -0300

    feat: added logic to calculate income and expenses and display them on the formatted screen

--- src/components/home/my-accounts/my-accounts.component.tsx
+++ src/components/home/my-accounts/my-accounts.component.tsx
@@ -1,3 +1,5 @@
+import { Fragment, FunctionComponent } from 'react';
+
 // Styles
 import {
   MyAccountsContainer,
@@ -19,65 +21,95 @@ import Title from '../../title/title.component';
 import CustomButton from '../../custom-button/custom-button.component';
 import GeneralBalance from '../general-balance/general-balance.component';
 
-const MyAccounts = () => {
+// Utilities
+import Account from '../../../types/account.types';
+import NextAccount from '../../../types/next-account.types';
+import { formatCurrencyWithSymbol } from '../../../utils/formatCurrency';
+
+interface MyAccountsProps {
+  accounts: Account[];
+  incomes: NextAccount[];
+  expenses: NextAccount[];
+}
+
+const MyAccounts: FunctionComponent<MyAccountsProps> = ({
+  accounts,
+  incomes,
+  expenses,
+}) => {
+  const groupedIncomes = incomes.reduce(
+    (acc, income) => {
+      if (income.processed) {
+        acc[income.account.id] = (acc[income.account.id] || 0) + income.value;
+      }
+      return acc;
+    },
+    {} as Record<string, number>
+  );
+
+  const totalIncomes = Object.values(groupedIncomes).reduce(
+    (acc, value) => acc + value,
+    0
+  );
+
+  const groupedExpenses = expenses.reduce(
+    (acc, expense) => {
+      if (expense.processed) {
+        acc[expense.account.id] =
+          (acc[expense.account.id] || 0) + expense.value;
+      }
+      return acc;
+    },
+    {} as Record<string, number>
+  );
+
+  const totalExpenses = Object.values(groupedExpenses).reduce(
+    (acc, value) => acc + value,
+    0
+  );
+
   return (
     <MyAccountsContainer>
-      <GeneralBalance title="Saldo geral" balance={0} />
+      <GeneralBalance
+        title="Saldo geral"
+        balance={totalIncomes - totalExpenses}
+        type="income"
+      />
       <MyAccountsDivisory></MyAccountsDivisory>
       <MyAccountsContent>
         <Title marginBottom="20px">Minhas contas</Title>
         <MyAccountsItems>
-          <MyAccountsItem>
-            <MyAccountsItemImgContainer>
-              <MyAccountsItemImg
-                src="/institutions/logos/carteira-inicial.png"
-                alt="Conta inicial"
-              />
-            </MyAccountsItemImgContainer>
-            <MyAccountsItemTitleContainer>
-              <MyAccountsItemTitle>Conta inicial</MyAccountsItemTitle>
-              <MyAccountsItemSubtitle>Conta manual</MyAccountsItemSubtitle>
-            </MyAccountsItemTitleContainer>
-            <MyAccountsItemBalanceContainer>
-              <MyAccountsItemBalance>R$ 0,00</MyAccountsItemBalance>
-            </MyAccountsItemBalanceContainer>
-          </MyAccountsItem>
+          {accounts.map((account, index) => {
+            const totalAccountIncomes = groupedIncomes[account.id] || 0;
+            const totalAccountExpenses = groupedExpenses[account.id] || 0;
 
-          <MyAccountsDivisory></MyAccountsDivisory>
+            const accountBalance = totalAccountIncomes - totalAccountExpenses;
 
-          <MyAccountsItem>
-            <MyAccountsItemImgContainer>
-              <MyAccountsItemImg
-                src="/institutions/logos/intermedium.png"
-                alt="Banco Inter"
-              />
-            </MyAccountsItemImgContainer>
-            <MyAccountsItemTitleContainer>
-              <MyAccountsItemTitle>Inter</MyAccountsItemTitle>
-              <MyAccountsItemSubtitle>Conta manual</MyAccountsItemSubtitle>
-            </MyAccountsItemTitleContainer>
-            <MyAccountsItemBalanceContainer>
-              <MyAccountsItemBalance>R$ 0,00</MyAccountsItemBalance>
-            </MyAccountsItemBalanceContainer>
-          </MyAccountsItem>
-
-          <MyAccountsDivisory></MyAccountsDivisory>
-
-          <MyAccountsItem>
-            <MyAccountsItemImgContainer>
-              <MyAccountsItemImg
-                src="/institutions/logos/nuconta.png"
-                alt="NuBank"
-              />
-            </MyAccountsItemImgContainer>
-            <MyAccountsItemTitleContainer>
-              <MyAccountsItemTitle>NuBank</MyAccountsItemTitle>
-              <MyAccountsItemSubtitle>Conta manual</MyAccountsItemSubtitle>
-            </MyAccountsItemTitleContainer>
-            <MyAccountsItemBalanceContainer>
-              <MyAccountsItemBalance>R$ 0,00</MyAccountsItemBalance>
-            </MyAccountsItemBalanceContainer>
-          </MyAccountsItem>
+            return (
+              <Fragment key={account.id}>
+                <MyAccountsItem>
+                  <MyAccountsItemImgContainer>
+                    <MyAccountsItemImg
+                      src={account.icon.src}
+                      alt={account.icon.alt}
+                    />
+                  </MyAccountsItemImgContainer>
+                  <MyAccountsItemTitleContainer>
+                    <MyAccountsItemTitle>{account.name}</MyAccountsItemTitle>
+                    <MyAccountsItemSubtitle>
+                      {account.type}
+                    </MyAccountsItemSubtitle>
+                  </MyAccountsItemTitleContainer>
+                  <MyAccountsItemBalanceContainer>
+                    <MyAccountsItemBalance>
+                      {formatCurrencyWithSymbol(accountBalance)}
+                    </MyAccountsItemBalance>
+                  </MyAccountsItemBalanceContainer>
+                </MyAccountsItem>
+                {accounts.length - 1 !== index && <MyAccountsDivisory />}
+              </Fragment>
+            );
+          })}
         </MyAccountsItems>
         <CustomButton>Gerenciar contas</CustomButton>
       </MyAccountsContent>
