dfa714f8c044d6ba6a501e51d17cfbc05295a031
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-14 12:06:51 -0300

    feat: create Next Account Type

--- src/types/next-account.types.ts
+++ src/types/next-account.types.ts
@@ -1,10 +1,14 @@
+import Account from './account.types';
 import Category from './category.types';
 
 interface NextAccount {
+  id: string;
   title: string;
   date: string;
   value: number;
   category: Category;
+  account: Account;
+  processed: boolean;
 }
 
 export default NextAccount;
