5fcd3233401991a8d5e1b29b64b16bbcacd3219e
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-16 14:15:05 -0300

    style: removed min height

--- src/components/card/card.styles.ts
+++ src/components/card/card.styles.ts
@@ -13,7 +13,6 @@ export const CardContainer = styled.div.withConfig({
   border: 1px solid var(--body-background-color);
   box-shadow: 0px 41px 70px rgba(0, 0, 0, 0.03);
   border-radius: 8px;
-  min-height: 180px;
   min-width: 150px;
   box-sizing: border-box;
   flex: 1 1 calc(${(props) => props.width} - 1rem);
