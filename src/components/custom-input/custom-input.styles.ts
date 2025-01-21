f9039fcb79b6da5b5c9cb491e661e177e5f626fa
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-20 21:04:28 -0300

    style: changed background and border style

--- src/components/custom-input/custom-input.styles.ts
+++ src/components/custom-input/custom-input.styles.ts
@@ -9,15 +9,17 @@ export const CustomInputContainer = styled.input.withConfig({
 })<CustomInputContainerProps>`
   border: none;
   width: 100%;
-  background-color: #e9ecef;
+  /* background-color: #e9ecef; */
+  border: 1px solid var(--line-color);
+  background-color: transparent;
   padding-right: 20px;
   padding-left: 20px;
   padding-top: 10px;
   padding-bottom: 10px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  /* box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); */
   border-radius: 10px;
   color: '#343a40';
-  border: ${(props) => (props.hasError ? '2px solid #FF6A6A' : 'none')};
+  /* border: ${(props) => (props.hasError ? '2px solid #FF6A6A' : 'none')}; */
 
   &::placeholder {
     color: ${(props) => (props.hasError ? '#FF6A6A' : '#6C757D')};
