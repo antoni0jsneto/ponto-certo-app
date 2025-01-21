3fa0be8c405854682d49c86e5ead931f7e7e5414
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-20 21:04:41 -0300

    feat: add Login Page

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,25 +1,50 @@
+import { FcGoogle } from 'react-icons/fc';
+import { FiLogIn } from 'react-icons/fi';
+
+// Components
 import CustomButton from '../../components/custom-button/custom-button.component';
+
+// Styles
 import {
   LoginContainer,
   LoginContent,
   LoginHeadline,
   LoginInputContainer,
   LoginSubtitle,
+  Logomarca,
 } from './login.styles';
+import CustomInput from '../../components/custom-input/custom-input.component';
+import { CgSpinnerTwo } from 'react-icons/cg';
 
 const LoginPage = () => {
   return (
     <LoginContainer>
+      <Logomarca>
+        <CgSpinnerTwo color="var(--green-color)" size={30} />
+        <p>ponto certo</p>
+      </Logomarca>
       <LoginContent>
         <LoginHeadline>Entre com a sua conta</LoginHeadline>
 
-        <CustomButton variant="success">Entrar</CustomButton>
+        <CustomButton variant="danger" startIcon={<FcGoogle size={18} />}>
+          Entrar com o Google
+        </CustomButton>
 
         <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
 
-        <LoginInputContainer></LoginInputContainer>
+        <LoginInputContainer>
+          <p style={{ color: '#4d4d4d' }}>Seu e-mail</p>
+          <CustomInput placeholder="Digite seu e-mail" />
+        </LoginInputContainer>
 
-        <CustomButton variant="primary">Entrar</CustomButton>
+        <LoginInputContainer>
+          <p style={{ color: '#4d4d4d' }}>Sua senha</p>
+          <CustomInput placeholder="Digite sua senha" />
+        </LoginInputContainer>
+
+        <CustomButton variant="success" startIcon={<FiLogIn size={18} />}>
+          Entrar
+        </CustomButton>
       </LoginContent>
     </LoginContainer>
   );
--- src/pages/login/login.styles.ts
+++ src/pages/login/login.styles.ts
@@ -1,17 +1,38 @@
 import styled from 'styled-components';
 
 export const LoginContainer = styled.div`
-  height: 100%;
+  height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  background-color: var(--body-background-color);
+`;
+
+export const Logomarca = styled.div`
+  font-family: 'Oxanium', serif;
+  font-weight: bold;
+  font-size: 1.6rem;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+  margin-bottom: 20px;
+
+  &:hover {
+    color: #d3f4dc;
+  }
+
+  p {
+    color: #2f473f;
+  }
 `;
 
 export const LoginHeadline = styled.p`
   font-weight: 600;
   font-size: 1.3rem;
   margin-bottom: 20px;
-  color: var(--text-primary-color);
+  color: #4d4d4d;
 `;
 
 export const LoginContent = styled.div`
@@ -19,12 +40,15 @@ export const LoginContent = styled.div`
   flex-direction: column;
   align-items: center;
   width: 450px;
+  background-color: #fffdf9;
+  border-radius: 6px;
+  padding: 43px 30px;
 `;
 
 export const LoginSubtitle = styled.p`
-  color: var(--text-primary-color);
+  color: #4d4d4d;
   padding-bottom: 20px;
-  border-bottom: 1px solid #6c757d;
+  border-bottom: 1px solid rgb(218, 218, 218);
   width: 100%;
   margin-top: 20px;
   text-align: center;
@@ -39,5 +63,6 @@ export const LoginInputContainer = styled.div`
   p:nth-child(1) {
     font-weight: 600;
     margin-bottom: 5px;
+    text-align: start;
   }
 `;
