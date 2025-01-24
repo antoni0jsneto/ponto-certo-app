078cd25d554a3be651dc90cd47d91d8d7f405a4a
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-24 11:34:11 -0300

    feat: add User Context

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Styles
+import './index.css';
+
+// Components
+import App from './App';
+
+// Utilities
+import UserContextProvider from './contexts/user.context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
   </React.StrictMode>
 );
 
