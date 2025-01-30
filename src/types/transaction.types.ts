5046fbc17a3dcd64c10f90060afcd9b8a1594d2a
Antonio Neto <antonio.silva6@yahoo.com.br>
2025-01-30 14:59:51 -0300

    feat: add Transaction Types

---
+++ src/types/transaction.types.ts
@@ -0,0 +1,29 @@
+import Account from './account.types';
+import Category from './category.types';
+
+interface Transaction {
+  id: string;
+  title: string;
+  date: string;
+  value: number;
+  category: Category;
+  account: Account;
+  processed: boolean;
+  type: 'income' | 'expense';
+  repeat?: {
+    frequency:
+      | 'daily'
+      | 'weekly'
+      | 'biweekly'
+      | 'monthly'
+      | 'bimonthly'
+      | 'quarterly'
+      | 'semestral'
+      | 'yearly';
+    type: 'fixed' | 'installment';
+  };
+  observation?: string;
+  attachments?: string[];
+}
+
+export default Transaction;
